e0c2b9ca8a85d6d3a5972878b8972f3a14a4c7f5
PabloCorsaro <pablo.gl.corsaro@gmail.com>
2017-05-29 16:15:20 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout << "Ingresa dos numeros" << endl;
+    cin >> a >> b;
+    cout << "La suma de ambos es: " << a+b << endl;
+    return 0;
+}
